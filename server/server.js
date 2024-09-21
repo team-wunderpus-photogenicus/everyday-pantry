78f7982fef388965199b09a72994eaea9c28ee92
Aaron Jacobs <aaronj313@gmail.com>
2024-09-21 09:57:29 -0700

    add minor changes to pull server changes

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ app.get('/', (req, res) => {
   return res.status(200).sendFile(path.join(__dirname, '../client/public/index.html'));
 })
 
+app.post('/', )
+
 
 
 app.listen(3000, () => console.log('Listening on port 3000.'));
