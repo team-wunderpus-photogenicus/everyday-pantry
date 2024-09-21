5851dcc97eb8eee44e18cf402ef08db2dfb2f738
James Appleton <jappleton101@gmail.com>
2024-09-20 20:32:28 -0400

    package json config progress

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './client/index.js',
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  // devtool: false,
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/preset-env', '@babel/preset-react'], },
+      },
+      {
+        test: /\.s?css/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: 'index.html',
+    }),
+  ],
+  // devServer: {
+  //   static: {
+  //     publicPath: '/build',
+  //     directory: path.resolve(__dirname, 'build'),
+  //   },
+  //   port: 8080,
+  //   proxy: [
+  //     {
+  //       context: ['/api'],
+  //       target: 'http://localhost:3000',
+  //     },
+  //   ],
+  // },
 };
