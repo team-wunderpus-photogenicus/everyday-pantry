505c10a82aee28c1508725691c2b66d5d449a121
agrimm17 <alexandergrimm17@gmail.com>
2024-09-23 07:57:13 -0700

    working recipelist

--- client/components/Recipe.jsx
+++ client/components/Recipe.jsx
@@ -3,20 +3,19 @@ import React from 'react';
 const Recipe = ({ recipe }) => {
   const { recipe_name, recipe_description, ingredients } = recipe;
 
-
   //Can a DELETE request be made by a button? Or maybe just a post with a Method of Delete?
+
   return (
     <div>
-        <form><button type="button">OFF</button></form> 
       <div>
         {recipe_name}: {recipe_description}
       </div>
-      <div>
-        {ingredients}
-      </div>
+      <div>Ingredients List: {ingredients.join(', ')}</div>
+      <form>
+        <button type='button'>DELETE</button>
+      </form>
     </div>
   );
 };
-
 
 export default Recipe;
