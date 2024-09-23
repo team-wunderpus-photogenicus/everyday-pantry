34869b5ffe295b258da7927e06fa5f720a0052ad
James Appleton <jappleton101@gmail.com>
2024-09-23 14:36:07 -0400

    add get ingredients get recipes endpoints

--- server/controller/recipeController.js
+++ server/controller/recipeController.js
@@ -1,31 +1,45 @@
-const pg = require('pg');
-const { Pool } = pg;
-
-const pool = new Pool();
+const db = require('../models/everdayPantryModels');
 
 const RecipeController = {};
 
 RecipeController.getRecipes = async (req, res, next) => {
   console.log('Retrieving all recipes');
   try {
-    const client = await pool.connect();
-    res.locals.recipes = await client.query('SELECT * FROM recipes'); // Join w/ingredients
+    res.locals.recipes = await db.query('SELECT * FROM recipes');
+    console.log('recipes', res.locals.recipes.rows);
+
+    for (let i = 0; i < res.locals.recipes.rows.length; i++) {
+      await db
+        .query(
+          `SELECT ri.recipe_id, i.ingredient_name FROM recipe_ingredients ri INNER JOIN ingredients i on ri.ingredient_id=i._id WHERE ri.recipe_id = '${res.locals.recipes.rows[i]._id}'`
+        )
+        .then(
+          (result) => (res.locals.recipes.rows[i].ingredients = result.rows)
+        );
+    }
+
+    await Promise.all(res.locals.recipes.rows);
+
+    console.log('recipes plus ingredients', res.locals.recipes.rows);
+    return next();
   } catch (e) {
+    console.log(e);
     return next({ error: e });
   }
-
-  return next();
 };
 
 RecipeController.getIngredients = async (req, res, next) => {
   console.log('Retrieving ingredients');
   try {
-    const client = await pool.connect();
-    res.locals.recipes = await client.query('SELECT * FROM ingredients');
+    const result = await db.query(
+      'SELECT COUNT(*) as count, ingredient_name as ingredient FROM ingredients GROUP BY ingredient_name'
+    );
+    res.locals.ingredients = result.rows;
+    console.log(res.locals.ingredients);
+    return next();
   } catch (e) {
     return next({ error: e });
   }
-  return next();
 };
 
 module.exports = RecipeController;
